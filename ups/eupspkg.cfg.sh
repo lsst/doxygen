1cebdda82e766550585ca78665ef8140b81c03ce
Joshua Hoblitt <josh@hoblitt.com>
2018-03-05 10:31:40 -0700

    do not exclude conda packages

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,9 +2,5 @@
 
 config()
 {
-# Prevent CMake from finding include files distributed in $(dirname
-# python)/../include. This is CMake's default behaviour, but can cause us to
-# erroneously use headers distributed by e.g. Anaconda.
-    PYTHONINCDIR=$(which python | sed -e's|bin/python|include|')
-    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_SYSTEM_IGNORE_PATH=${PYTHONINCDIR}
+    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}
 }
