3e94a895bde9b5a5ca146e9c976170a6d0eb4ce3
Joshua Hoblitt <josh@hoblitt.com>
2018-04-06 09:11:14 -0700

    fix shellcheck warnings

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # EupsPkg config file. Sourced by 'eupspkg'
 
 config()
@@ -6,5 +8,5 @@ config()
 # python)/../include. This is CMake's default behaviour, but can cause us to
 # erroneously use headers distributed by e.g. Anaconda.
     PYTHONINCDIR=$(which python | sed -e's|bin/python|include|')
-    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_SYSTEM_IGNORE_PATH=${PYTHONINCDIR}
+    cmake . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_SYSTEM_IGNORE_PATH="${PYTHONINCDIR}"
 }
